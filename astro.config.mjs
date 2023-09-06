8170d5e33890a3e62cfa54110a38c5dc71ae7fb8
thormengkheang <thormengkheang@gmail.com>
2023-09-06 15:07:19 +0700

    remove twitter & update github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site: process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
+  site:
+    process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), tailwind()],
   vite: {
