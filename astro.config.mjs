88fc9dbb3f1f943562501ad2de2515c2d541a846
thormengkheang <thormengkheang@gmail.com>
2023-09-13 13:37:46 +0700

    add google analytic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thormengkheang.github.io',
   trailingSlash: 'ignore',
-  integrations: [ mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js']
