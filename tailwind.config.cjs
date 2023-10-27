7814b239477f1e6a34519d607533ed867e362b16
thormengkheang <thormengkheang@gmail.com>
2023-10-27 08:33:38 +0700

    add links & skills

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,6 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [
-		require('@tailwindcss/line-clamp'),
 		require('@tailwindcss/typography'),
 		iconsPlugin({
 			// Select the icon collections you want to use
